9c867ac28fecf7d43899ab2a33710e4bb549901d
Musalu Wambua <47148208+wambua-iv@users.noreply.github.com>
2022-11-18 07:23:41 +0000

    added graph component

--- src/components/homepage/Chart.tsx
+++ src/components/homepage/Chart.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { LineChart, XAxis, Tooltip, CartesianGrid, Line } from "recharts";
 
-const data = [{ name: "Page A", uv: 400, pv: 2400, amt: 2400 }];
+const data = [
+  { name: "Page A", death: 400, pv: 200, lives: 70, amt: 2400 },
+  { name: "Page b", death: 500, pv: 20, lives: 900, amt: 2400 },
+  { name: "Page c", death: 300, pv: 200, lives: 700, amt: 2400 },
+  { name: "Page d", death: 600, pv: 2400, lives: 600, amt: 2400 },
+];
 
 function Chart() {
   return (
@@ -11,11 +16,18 @@ function Chart() {
       data={data}
       margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
     >
+     <LineChart
+      width={400}
+      height={400}
+      data={data}
+      margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+    ></LineChart>
       <XAxis dataKey="name" />
       <Tooltip />
       <CartesianGrid stroke="#fff" />
-      <Line type="monotone" dataKey="uv" stroke="#ffa300" yAxisId={0} />
+      <Line type="monotone" dataKey="death" stroke="#ffa300" yAxisId={0} />
       <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+      <Line type="monotone" dataKey="lives" stroke="#387908" yAxisId={1} />
     </LineChart>
   );
 }
